e37d0eefb4e6e392a3357b8f835cd146064d2a17
sanjaykumarR07 <rsanjaykumar1319@gmail.com>
2025-04-10 16:23:41 +0530

    qr-change

--- js/register.js
+++ js/register.js
@@ -24,12 +24,20 @@ document.getElementById('registerForm').addEventListener('submit', function (e) 
     localStorage.setItem('users', JSON.stringify(users));
   
     document.getElementById('qrcode').innerHTML = '';
-    new QRCode(document.getElementById('qrcode'), {
-      text: id,
-      width: 200,
-      height: 200,
+    // new QRCode(document.getElementById('qrcode'), {
+    //   text: id,
+    //   width: 400,
+    //   height: 400,
+    // });
+
+    new QRCode(document.getElementById("qrcode"), {
+      text: user.id,
+      width: 250, // or 300 for even bigger
+      height: 250,
+      colorDark: "#ffffff",
+      colorLight: "transparent",
+      correctLevel: QRCode.CorrectLevel.H
     });
-  
     Swal.fire('Success', 'Registered successfully & QR code generated.', 'success');
     e.target.reset();
   });
